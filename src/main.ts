2c50591351d9d2f8908e39816a52cdd5d8af9e51
MuhammadAhmad338 <ahmadmuhammad.7700@gmail.com>
2026-05-23 15:42:21 +0500

    latest commits

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT ?? 3000;
+  app.enableCors();
+  
+  app.use(bodyParser.json({ limit: '10mb' }));
+  app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
